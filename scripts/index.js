b5557dc2c1c3f434fe76313f978ab6600591d82b
Alekseenko Andrey <alekseenko.developer@yandex.ru>
2022-05-29 22:25:09 +0300

    Вторая итерация Проектной работы 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
-let savePopupButton = document.querySelector('.popup__input-button');
-let nameValue = document.querySelector('.popup__input-text_name');
-let aboutValue = document.querySelector('.popup__input-text_about');
+let popupForm = document.querySelector('.popup__form');
+let nameValue = document.querySelector('.popup__input-text_content_name');
+let aboutValue = document.querySelector('.popup__input-text_content_about');
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
 
 openPopupButton.onclick = function() {
     nameValue.value = profileName.textContent;
     aboutValue.value = profileAbout.textContent;
-    popup.classList.add('popup_opened');
+    popup.classList.add('popup_state_opened');
 }
 
 closePopupButton.onclick = function() {
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_state_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameValue.value;
     profileAbout.textContent = aboutValue.value;
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_state_opened');
 }
 
-savePopupButton.addEventListener('click', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler);
